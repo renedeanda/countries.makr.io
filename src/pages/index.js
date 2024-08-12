f61758a08db28b6ad3811a514be36a59a8660980
René DeAnda <rene.ideanda@gmail.com>
2024-08-12 13:15:16 -0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,9 @@ import CountryExplorer from '../components/CountryExplorer';
 
 export default function Home() {
   return (
-    <>
+    <div className="min-h-screen bg-gradient-to-b from-blue-100 to-blue-200">
       <SEOMetadata />
       <CountryExplorer />
-    </>
+    </div>
   );
 }
