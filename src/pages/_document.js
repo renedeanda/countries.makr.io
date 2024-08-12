7e4aaee2b7b9849425a23f390ed8c2871d8ba678
René DeAnda <rene.ideanda@gmail.com>
2024-08-12 13:07:38 -0700

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,7 +5,9 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-        <meta name="theme-color" content="#BFDBFE" />
+       <meta name="theme-color" content="#BFDBFE" media="(prefers-color-scheme: light)" />
+          <meta name="theme-color" content="#1E3A8A" media="(prefers-color-scheme: dark)" />
+          <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
       </Head>
       <body>
         <Main />
